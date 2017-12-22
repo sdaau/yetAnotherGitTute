5edfe55511d804d470270796282ccf40c2220424
sdaau <sdaau@wherever>
2017-12-22 15:00:56 +0100

    : fixup montage-screenshots.py

--- montage-screenshots.py
+++ montage-screenshots.py
@@ -35,16 +35,14 @@ OUTDIR="img"
 import Image
 row_size = 2 # how many elems in row
 col_size = 2
-margin = 5
+margin = 10
 def generate_montage(filenames, output_fn):
   images = [Image.open(filename) for filename in filenames]
 
   width = margin + max(image.size[0] + margin for image in images)*row_size
   height = margin + max(image.size[1] + margin for image in images)*col_size
-  montage = Image.new(mode='RGBA', size=(width, height), color=(180,180,180,255))
+  montage = Image.new(mode='RGBA', size=(width, height), color=(20,20,20,255))
 
-  #~ max_x = 0
-  #~ max_y = 0
   offset_x = 0
   offset_y = 0
 
@@ -61,20 +59,10 @@ def generate_montage(filenames, output_fn):
 
     montage.paste(image, (offset_x, offset_y))
 
-    #~ max_x = max(max_x, offset_x + image.size[0])
-    #~ max_y = max(max_y, offset_y + image.size[1])
-
-    #~ if i % row_size == row_size-1:
-      #~ offset_y = max_y + margin
-      #~ offset_x = 0
-    #~ else:
-      #~ offset_x += margin + image.size[0]
-
-  #~ montage = montage.crop((0, 0, max_x, max_y))
   montage.save(output_fn)
 
 
-
+###################### start:
 
 # glob all the .png files in indir (assumming ALL are like _1, _2, _3)
 all_indir_pngs = glob.glob('{}/*.png'.format(INDIR))
@@ -100,3 +88,39 @@ for i, ubname in enumerate(uniq_prim_names):
   outname = os.path.join( OUTDIR, "{}.png".format(ubname) )
   print( "{}: {}".format(outname, myubnameset) )
   generate_montage(myubnameset, outname)
+
+
+
+
+"""
+script prints out:
+
+img/scrshot_001.png: ['imgscrshotB/scrshot_001_1.png', 'imgscrshotB/scrshot_001_2.png', 'imgscrshotB/scrshot_001_3.png']
+img/scrshot_002.png: ['imgscrshotB/scrshot_002_1.png', 'imgscrshotB/scrshot_002_2.png', 'imgscrshotB/scrshot_002_3.png']
+img/scrshot_003.png: ['imgscrshotB/scrshot_003_1.png', 'imgscrshotB/scrshot_003_2.png', 'imgscrshotB/scrshot_003_3.png']
+img/scrshot_004.png: ['imgscrshotB/scrshot_004_1.png', 'imgscrshotB/scrshot_004_2.png', 'imgscrshotB/scrshot_004_3.png']
+img/scrshot_005.png: ['imgscrshotB/scrshot_005_1.png', 'imgscrshotB/scrshot_005_2.png', 'imgscrshotB/scrshot_005_3.png']
+img/scrshot_006.png: ['imgscrshotB/scrshot_006_1.png', 'imgscrshotB/scrshot_006_2.png', 'imgscrshotB/scrshot_006_3.png']
+img/scrshot_007.png: ['imgscrshotB/scrshot_007_1.png', 'imgscrshotB/scrshot_007_2.png', 'imgscrshotB/scrshot_007_3.png']
+img/scrshot_008.png: ['imgscrshotB/scrshot_008_1.png', 'imgscrshotB/scrshot_008_2.png', 'imgscrshotB/scrshot_008_3.png']
+img/scrshot_009.png: ['imgscrshotB/scrshot_009_1.png', 'imgscrshotB/scrshot_009_2.png', 'imgscrshotB/scrshot_009_3.png']
+img/scrshot_010.png: ['imgscrshotB/scrshot_010_1.png', 'imgscrshotB/scrshot_010_2.png', 'imgscrshotB/scrshot_010_3.png']
+img/scrshot_011.png: ['imgscrshotB/scrshot_011_1.png', 'imgscrshotB/scrshot_011_2.png', 'imgscrshotB/scrshot_011_3.png']
+img/scrshot_012.png: ['imgscrshotB/scrshot_012_1.png', 'imgscrshotB/scrshot_012_2.png', 'imgscrshotB/scrshot_012_3.png']
+img/scrshot_013.png: ['imgscrshotB/scrshot_013_1.png', 'imgscrshotB/scrshot_013_2.png', 'imgscrshotB/scrshot_013_3.png']
+img/scrshot_014.png: ['imgscrshotB/scrshot_014_1.png', 'imgscrshotB/scrshot_014_2.png', 'imgscrshotB/scrshot_014_3.png']
+img/scrshot_015.png: ['imgscrshotB/scrshot_015_1.png', 'imgscrshotB/scrshot_015_2.png', 'imgscrshotB/scrshot_015_3.png']
+img/scrshot_016.png: ['imgscrshotB/scrshot_016_1.png', 'imgscrshotB/scrshot_016_2.png', 'imgscrshotB/scrshot_016_3.png']
+img/scrshot_017.png: ['imgscrshotB/scrshot_017_1.png', 'imgscrshotB/scrshot_017_2.png', 'imgscrshotB/scrshot_017_3.png']
+img/scrshot_018.png: ['imgscrshotB/scrshot_018_1.png', 'imgscrshotB/scrshot_018_2.png', 'imgscrshotB/scrshot_018_3.png']
+img/scrshot_019.png: ['imgscrshotB/scrshot_019_1.png', 'imgscrshotB/scrshot_019_2.png', 'imgscrshotB/scrshot_019_3.png']
+img/scrshot_020.png: ['imgscrshotB/scrshot_020_1.png', 'imgscrshotB/scrshot_020_2.png', 'imgscrshotB/scrshot_020_3.png']
+img/scrshot_021.png: ['imgscrshotB/scrshot_021_1.png', 'imgscrshotB/scrshot_021_2.png', 'imgscrshotB/scrshot_021_3.png']
+img/scrshot_022.png: ['imgscrshotB/scrshot_022_1.png', 'imgscrshotB/scrshot_022_2.png', 'imgscrshotB/scrshot_022_3.png']
+img/scrshot_023.png: ['imgscrshotB/scrshot_023_1.png', 'imgscrshotB/scrshot_023_2.png', 'imgscrshotB/scrshot_023_3.png']
+img/scrshot_024.png: ['imgscrshotB/scrshot_024_1.png', 'imgscrshotB/scrshot_024_2.png', 'imgscrshotB/scrshot_024_3.png']
+img/scrshot_025.png: ['imgscrshotB/scrshot_025_1.png', 'imgscrshotB/scrshot_025_2.png', 'imgscrshotB/scrshot_025_3.png']
+img/scrshot_026.png: ['imgscrshotB/scrshot_026_1.png', 'imgscrshotB/scrshot_026_2.png', 'imgscrshotB/scrshot_026_3.png']
+img/scrshot_027.png: ['imgscrshotB/scrshot_027_1.png', 'imgscrshotB/scrshot_027_2.png', 'imgscrshotB/scrshot_027_3.png']
+
+"""
